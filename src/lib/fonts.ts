c81aa2715a43dfa1aa107e4e3adef02c63878d34
gomezdn <gomezdn@outlook.com>
2025-03-31 18:01:37 -0300

    feat: add fonts file

---
+++ src/lib/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lato } from 'next/font/google'
+
+export const inter = Inter({
+    subsets: ['latin'],
+    weight: ['300', '400', '600', '700']
+})
+  
+export const lato = Lato({
+    subsets: ['latin'],
+    weight: ['700']
+})
