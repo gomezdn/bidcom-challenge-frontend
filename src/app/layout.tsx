187bb781f9c359f06c3c50d54d4c49607d5c4c4e
gomezdn <gomezdn@outlook.com>
2025-03-31 18:03:48 -0300

    feat: add home page with all components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,9 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "Bidcom challenge landing",
-  description: "A landing page made with Next.js for a Bidcom challenge",
+  title: 'Bidcom challenge landing',
+  description: 'A landing page made with Next.js for a Bidcom challenge',
 };
 
 export default function RootLayout({
@@ -23,10 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang='en'>
+      <body>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,46 @@
-export default function Home() {
+import Image from 'next/image'
+import Link from 'next/link'
+import { inter, lato } from '@/lib/fonts'
+import { getCarouselData, getHomepageData } from '@/lib/services'
+import leftArrow from '@public/button-arrow-left-icon.svg'
+import icon from '@public/icon.svg'
+import menuICon from '@public/menu-icon.svg'
+import HeroSlider from '@/components/HeroSlider'
+
+export default async function Home() {
+  const carouselImages = await getCarouselData()
+
+  const {
+    headline,
+    title,
+    description,
+    button: { 
+      title: buttonTitle,
+      link: buttonLink
+    }
+  } = await getHomepageData()
+
   return (
-    <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      </main>
-    </div>
-  );
+    <main className='flex flex-col w-screen min-h-screen py-[64px] md:px-[72px] px-3 justify-between items-center'>
+      <Image src={menuICon} width={32} height={32} className='self-end cursor-pointer' alt='menu icon'/>
+
+      <div className='m-auto flex md:flex-row flex-col w-full md:gap-0 gap-4 justify-around items-center'>
+        <div className='md:flex flex-col gap-4 w-[369px] md:w-[593px] h-[286px] md:text-left text-center contents'>
+          <div className={`flex flex-col gap-4 items-center md:items-start ${inter.className}`}>
+            <p className='text-xs md:text-sm font-[400]'>{headline.replace('--', '—')}</p>
+            <h1 className={`font-[400] text-3xl md:text-5xl`} dangerouslySetInnerHTML={{__html: '¡' + title}}/>
+            <p className='font-[300] text-base text-left'>{description + description + description}</p>
+          </div>
+          <Link className='order-last w-full' href={buttonLink}>
+            <div className='flex justify-center gap-2 font-[700] w-full md:w-[157px] h-[54px] text-sm bg-black rounded-[10px] text-white'>
+              <button className={`${lato.className}`}>{buttonTitle}</button>
+              <Image src={leftArrow} width={12} height={12} alt='Arrow icon indicating to follow link'/>
+            </div>
+          </Link>
+        </div>
+        <HeroSlider images={carouselImages}/>
+      </div>
+      <Image src={icon} width={40} height={40} className='self-center cursor-pointer' alt='logo icon'/>
+    </main>
+  )
 }
